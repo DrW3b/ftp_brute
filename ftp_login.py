819c99c844d9583f998703896bc60f1db816d545
DrW3b <94005355+DrW3b@users.noreply.github.com>
2023-04-26 17:50:05 +0200

    Create ftp_login.py

---
+++ ftp_login.py
@@ -0,0 +1,40 @@
+import argparse
+import asyncio
+from ftplib import FTP
+
+async def attempt_login(server, username, password):
+    try:
+        ftp = FTP(server, timeout=10)
+        ftp.login(username, password)
+        print(f'Successful login to {server} with {username}:{password}')
+        ftp.quit()
+    except Exception as e:
+        print(f'Failed login attempt to {server} with {username}:{password}. {e}')
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--server-file', required=True, help='file containing server addresses')
+    parser.add_argument('-u', '--username-file', required=True, help='file containing usernames')
+    parser.add_argument('-p', '--password-file', required=True, help='file containing passwords')
+    args = parser.parse_args()
+
+    with open(args.server_file) as f_servers:
+        servers = f_servers.read().splitlines()
+
+    with open(args.username_file) as f_usernames:
+        usernames = f_usernames.read().splitlines()
+
+    with open(args.password_file) as f_passwords:
+        passwords = f_passwords.read().splitlines()
+
+    tasks = []
+    for server in servers:
+        for username in usernames:
+            for password in passwords:
+                task = asyncio.create_task(attempt_login(server, username, password))
+                tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    asyncio.run(main())
